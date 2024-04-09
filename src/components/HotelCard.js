aef409789850a1756c959210aebefcfdc4e9afa0
dylstar135 <dylstar160990@googlemail.com>
2024-04-09 17:05:59 +0100

    hotel description and toggle added

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/HotelCard.css";
 
-const HotelCard = ({ name, location, image, price, description, rating }) => {
+const HotelCard = ({
+  name,
+  location,
+  image,
+  price,
+  description,
+  hotelDescription,
+  rating,
+}) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="hotel-card-container">
       <img src={image} alt="name" />
       <div className="hotel">
         <h2>{name}</h2>
         <p className="location">{location}</p>
+        <button className="toggle-button" onClick={toggleDescription}>
+          {expanded ? "Hide Hotel Description" : "Show Hotel Description"}
+        </button>
+        <div className={`description-container ${expanded ? "expanded" : ""}`}>
+          <p className="description">
+            {expanded ? hotelDescription : description}
+          </p>
+        </div>
         <p>
           <strong>Rating:</strong>
           <span className="stars">{rating}</span>
         </p>
-        <p className="description">{description}</p>
         <div className="price-button-container">
           <button className="price-button">
             <span className="book-now">Book Now</span>
